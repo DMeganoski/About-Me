6814ccb892e2235a4b490462acce744c38a09bac
dmeganoski <dmeganoski@dmeganoski-ubuntu>
2012-01-26 00:54:31 -0500

    First Commit of ZeFish's Changes

--- default.php
+++ default.php
@@ -11,128 +11,121 @@ $PluginInfo['AboutMe'] = array(
 
 );
 
-class AboutMe extends Gdn_Plugin {
+class AboutMe implements Gdn_IPlugin {
 
-	/**
-	 * Add to profile @ tabs - link to view page.
-	 * If you would like this to be the first 'tab' on the list, go to
-	 * 'vanilla_folder/applications/dashboard/controllers/class.profilecontroller.php'
-	 * and add a
-	 * $this->FireEvent('BeforeAddProfileTabs');
-	 * somewhere around line 736 of the file,
-	 * and change the name of the magic event below to ProfileController_BeforeAddProfileTabs_handler
-	 */
-	public function ProfileController_AddProfileTabs_handler(&$Sender) {
-	   $Sender->AddProfileTab('aboutme', "/profile/aboutme/".$Sender->User->UserID."/".Gdn_Format::Url($Sender->User->Name), 'AboutMe', 'About Me');
-	   $Sender->AddCssFile('/plugins/AboutMe/design/am.default_theme.css');
-	}
+//==========Random Variables for later Use
+//$Controller = $Sender->ControllerName;
 
-
-	/**
-	 * Adds the button to the side menu for filling in info
-	 * @param $Sender
-	 */
-	public function ProfileController_AfterAddSideMenu_Handler(&$Sender) {
-		$SideMenu = $Sender->EventArguments['SideMenu'];
-		$Session = Gdn::Session();
-		$ViewingUserID = $Session->UserID;
-
-		if ($Sender->User->UserID == $ViewingUserID) {
-			$SideMenu->AddLink('Options', T('Edit My Details'), '/profile/editme/'.$Sender->User->UserID.'/'.Gdn_Format::Url($Sender->User->Name), FALSE, array('class' => 'Popup'));
-		} else {
-			$SideMenu->AddLink('Options', T('Edit My Details'), '/profile/editme/'.$Sender->User->UserID.'/'.Gdn_Format::Url($Sender->User->Name), 'Garden.Users.Edit', array('class' => 'Popup'));
-			return;
-		}
-	}
+/**
+ * Add to profile @ tabs - link to view page.
+ * If you would like this to be the first 'tab' on the list, go to
+ * 'vanilla_folder/applications/dashboard/controllers/class.profilecontroller.php'
+ * and add a
+ * $this->FireEvent('BeforeAddProfileTabs');
+ * somewhere around line 736 of the file,
+ * and change the name of the magic event below to ProfileController_BeforeAddProfileTabs_handler
+ */
+public function ProfileController_AddProfileTabs_handler(&$Sender) {
+   $Sender->AddProfileTab('aboutme', "/profile/aboutme/".$Sender->User->UserID."/".Gdn_Format::Url($Sender->User->Name), 'AboutMe', 'About Me');
+   $Sender->AddCssFile('/plugins/AboutMe/design/am.default_theme.css');
+}
 
 
-	/**
-	* Create a controller for 'edit' or 'view' commands
-	* @param unknown_type $Sender
-	* @param unknown_type $params - three elements - ['edit' or 'view' | user_id | user_name]
-	*/
-	public function ProfileController_AboutMe_Create(&$Sender, $params) {
-		$Sender->UserID = ArrayValue(0, $Sender->RequestArgs, '');
-		$Sender->UserName = ArrayValue(1, $Sender->RequestArgs, '');
-		$Sender->GetUserInfo($Sender->UserID, $Sender->UserName);
-		$Sender->SetTabView('aboutme', dirname(__FILE__).DS.'views'.DS.'aboutme_view.php', 'Profile', 'Dashboard');
-		   //$UserInfoModule = new UserInfoModule($this);
-		   //$UserInfoModule->User = $this->User;
-		   //$UserInfoModule->Roles = $this->Roles;
+/**
+ * Adds the button to the side menu for filling in info
+ * @param $Sender
+ */
+public function ProfileController_AfterAddSideMenu_Handler(&$Sender) {
+      $SideMenu = $Sender->EventArguments['SideMenu'];
+      $Session = Gdn::Session();
+      $ViewingUserID = $Session->UserID;
 
-		$AboutMeModel = new Gdn_Model('AboutMe');
-		$AboutMeData = $AboutMeModel->GetWhere(array('UserID' => $Sender->UserID));
-		$AboutMe = $AboutMeData->FirstRow();
-		$Sender->AboutMe = $AboutMe;
+      if ($Sender->User->UserID == $ViewingUserID) {
+         $SideMenu->AddLink('Options', T('Edit My Details'), '/profile/editme/'.$Sender->User->UserID.'/'.Gdn_Format::Url($Sender->User->Name), FALSE, array('class' => 'Popup'));
+      } else {
+      	$SideMenu->AddLink('Options', T('Edit My Details'), '/profile/editme/'.$Sender->User->UserID.'/'.Gdn_Format::Url($Sender->User->Name), 'Garden.Users.Edit', array('class' => 'Popup'));
+      	return;
+      }
+}
 
-		$Sender->HandlerType = HANDLER_TYPE_NORMAL;
-		$Sender->Render();
 
-	}
+/**
+* Create a controller for 'edit' or 'view' commands
+* @param unknown_type $Sender
+* @param unknown_type $params - three elements - ['edit' or 'view' | user_id | user_name]
+*/
+public function ProfileController_AboutMe_Create(&$Sender, $params) {
+   $Sender->UserID = ArrayValue(0, $Sender->RequestArgs, '');
+   $Sender->UserName = ArrayValue(1, $Sender->RequestArgs, '');
+   $Sender->GetUserInfo($Sender->UserID, $Sender->UserName);
+   $Sender->SetTabView('aboutme', dirname(__FILE__).DS.'views'.DS.'aboutme_view.php', 'Profile', 'Dashboard');
+       //$UserInfoModule = new UserInfoModule($this);
+       //$UserInfoModule->User = $this->User;
+       //$UserInfoModule->Roles = $this->Roles;
 
-	public function ProfileController_EditMe_Create(&$Sender, $params) {
+   $AboutMeModel = new Gdn_Model('AboutMe');
+   $AboutMeData = $AboutMeModel->GetWhere(array('UserID' => $Sender->UserID));
+   $AboutMe = $AboutMeData->FirstRow();
+   $Sender->AboutMe = $AboutMe;
 
-		$this->UserID = ArrayValue(0, $Sender->RequestArgs, '');
-		$this->UserName = ArrayValue(1, $Sender->RequestArgs, '');
-		$Sender->GetUserInfo($Sender->UserID, $Sender->UserName);
-		// change this to use a different css stylesheet i.e. from am.default_theme.css to am.realgamerstheme.css
-		$Sender->AddCssFile('/plugins/AboutMe/design/am.default_theme.css');
+   $Sender->HandlerType = HANDLER_TYPE_NORMAL;
+   $Sender->Render();
 
-		if (!is_numeric($this->UserID)) {
+}
 
-		  echo '<div class="Empty">Oops, thats not right...</div>';
-		  $Sender->Render();
-		}
-		$AboutMeModel = new Gdn_Model('AboutMe');
-		$AboutMeData = $AboutMeModel->GetWhere(array('UserID' => $this->UserID));
-		$AboutMe = $AboutMeData->FirstRow();
-		$Sender->AboutMe = $AboutMe;
+public function ProfileController_EditMe_Create(&$Sender, $params) {
+   $this->UserID = ArrayValue(0, $Sender->RequestArgs, '');
+   $this->UserName = ArrayValue(1, $Sender->RequestArgs, '');
+   $Sender->GetUserInfo($Sender->UserID, $Sender->UserName);
+   // change this to use a different css stylesheet i.e. from am.default_theme.css to am.realgamerstheme.css
+   $Sender->AddCssFile('/plugins/AboutMe/design/am.default_theme.css');
 
-		$Session = Gdn::Session();
-		$ViewingUserID = $Session->UserID;
-		if ($Sender->User->UserID != $ViewingUserID) {
-			$Sender->Permission('Garden.Users.Edit');
-			$AboutMeUserID = $Sender->User->UserID;
-		} else {
-			$AboutMeUserID = $ViewingUserID = Gdn::Session()->UserID;
-		}
+   if (!is_numeric($this->UserID)) {
 
-		//$Sender->AddSideMenu('plugin/userlist');
-		$Sender->Form = new Gdn_Form();
-		$Sender->Form->SetModel($AboutMeModel);
-		if ($Sender->Form->AuthenticatedPostBack() === FALSE) {
-			//AND If the table is empty, set the form data
-			if(!empty($Sender->AboutMe)) {
-				$Sender->Form->SetData($Sender->AboutMe);
-			}
-		} else { //starts save form
-			if(!empty($Sender->AboutMe)) {
-				$Sender->Form->SetFormValue('ProfileID', $Sender->AboutMe->ProfileID);
-			}
-			$Sender->Form->SetFormValue('UserID', $this->UserID);
-			$Data = $Sender->Form->FormValues();
-			if ($Sender->Form->Save() !== FALSE) {
-				$Sender->StatusMessage = Gdn::Translate("Your settings have been saved.");
-			// Trying to record activity if the person changed his/her info
-			// AddActivity($AboutMeUserID, 'Story', 'has updated his profile');
-		} else {
-			$Sender->StatusMessage = T("Oops, changes not saved");
-		}
-	} //ends save form
-	$Sender->View = dirname(__FILE__).DS.'views'.DS.'aboutme_edit.php';
+      echo '<div class="Empty">Oops, thats not right...</div>';
+      $Sender->Render();
+   }
+   $AboutMeModel = new Gdn_Model('AboutMe');
+   $AboutMeData = $AboutMeModel->GetWhere(array('UserID' => $this->UserID));
+   $AboutMe = $AboutMeData->FirstRow();
+   $Sender->AboutMe = $AboutMe;
 
-	//$Sender->HandlerType = HANDLER_TYPE_NORMAL;
-	$Sender->Render();
-	}
+   $Session = Gdn::Session();
+   $ViewingUserID = $Session->UserID;
+   if ($Sender->User->UserID != $ViewingUserID) {
+      $Sender->Permission('Garden.Users.Edit');
+      $AboutMeUserID = $Sender->User->UserID;
+   } else {
+      $AboutMeUserID = $ViewingUserID = Gdn::Session()->UserID;
+   }
 
-	/*
-	 * This method gets called in place of the EntryController's Render method.
-	 */
-	public function EntryController_AfterPasswordField_handler(&$Sender) {
-		echo '<li>';
-		echo $this->Form->Label('Address', 'Address');
-		echo $this->Form->TextBox('Address');
-    }
+   //$Sender->AddSideMenu('plugin/userlist');
+   $Sender->Form = new Gdn_Form();
+   $Sender->Form->SetModel($AboutMeModel);
+   if ($Sender->Form->AuthenticatedPostBack() === FALSE) {
+      //AND If the table is empty, set the form data
+      if(!empty($Sender->AboutMe)) {
+	 $Sender->Form->SetData($Sender->AboutMe);
+      }
+   } else { //starts save form
+      if(!empty($Sender->AboutMe)) {
+	 $Sender->Form->SetFormValue('ProfileID', $Sender->AboutMe->ProfileID);
+      }
+      $Sender->Form->SetFormValue('UserID', $this->UserID);
+      $Data = $Sender->Form->FormValues();
+      if ($Sender->Form->Save() !== FALSE) {
+	 $Sender->StatusMessage = Gdn::Translate("Your settings have been saved.");
+         // Trying to record activity if the person changed his/her info
+         // AddActivity($AboutMeUserID, 'Story', 'has updated his profile');
+      } else {
+	 $Sender->StatusMessage = T("Oops, changes not saved");
+      }
+   } //ends save form
+   $Sender->View = dirname(__FILE__).DS.'views'.DS.'aboutme_edit.php';
+
+   //$Sender->HandlerType = HANDLER_TYPE_NORMAL;
+   $Sender->Render();
+}
 
 /*public function ProfileController_AfterPreferencesDefined_Handler(&$Sender) {
    $Sender->Preferences['Profile Information Visibility'] = array(
@@ -147,7 +140,11 @@ class AboutMe extends Gdn_Plugin {
       'Pulbic.Birthyear' => T('Just hide the year.'),
       'Pulbic.HideSome' => T('Hide my info from the public. (unregistered users)'),
 
+
+
+
       );
+
 
 }*/
 /**
@@ -157,60 +154,38 @@ class AboutMe extends Gdn_Plugin {
  * Remember you would also have to add fields for new columns in the edit, and view pages as well.
  *
  */
-
-	public function Structure() {
-      $Structure = Gdn::Structure();
-      $Structure->Table('User')
-			->Column('Address', 'varchar(128)', TRUE)
-			->Column('City', 'varchar(64)', TRUE)
-			->Column('State', 'varchar(64)', TRUE)
-			->Column('Phone', 'varchar(12)', TRUE)
-			->Column('FirstName', 'varchar(64)', TRUE)
-			->Column('LastName', 'varchar(64)', TRUE)
-			->Set();
-
-	  /*
+public function Setup(){
+		$Structure = Gdn::Structure();
 		$Structure->Table('AboutMe')
-			->PrimaryKey('ProfileID')
-			->Column('UserID', 'int', FALSE, 'primary')
-			->Column('OtName', 'varchar(32)', TRUE)
-			->Column('RelStat', array('s','m','i','d','w'), FALSE)
-			->Column('BD', 'date', FALSE)
-			->Column('HideBD', array('1','0'), TRUE)
-			->Column('HideBY', array('1','0'), TRUE)
-			->Column('Quote', 'varchar(128)', TRUE)
-			// Location
-			->Column('Loc', 'varchar(32)', TRUE)
-			// Career and education
-			->Column('Emp', 'varchar(32)', TRUE)
-			->Column('JobTit', 'varchar(32)', TRUE)
-			->Column('HS', 'varchar(32)', TRUE)
-			->Column('Col', 'varchar(32)', TRUE)
-			// Interest fields
-			->Column('Inter', 'varchar(128)', TRUE)
-			->Column('Mus', 'varchar(128)', TRUE)
-			->Column('Gam', 'varchar(128)', TRUE)
-			->Column('Mov', 'varchar(128)', TRUE)
-			->Column('TV', 'varchar(128)', TRUE)
-			->Column('Bks', 'varchar(128)', TRUE)
-			->Column('Bio', 'text', TRUE)
-			->Set(FALSE, FALSE);
-			*/
-   }
-
-   public function Setup() {
-      $this->Structure();
-   }
-
-	public function OnDisable() {
-
+		->PrimaryKey('ProfileID')
+        ->Column('UserID', 'int', FALSE, 'primary')
+        ->Column('RealName', 'varchar(64)', FALSE)
+        ->Column('OtName', 'varchar(32)', TRUE)
+        ->Column('RelStat', array('s','m','i','d','w'), FALSE)
+        ->Column('BD', 'date', FALSE)
+        ->Column('HideBD', array('1','0'), TRUE)
+        ->Column('HideBY', array('1','0'), TRUE)
+        ->Column('Quote', 'varchar(128)', TRUE)
+        ->Column('Loc', 'varchar(32)', TRUE)
+        ->Column('Emp', 'varchar(32)', TRUE)
+        ->Column('JobTit', 'varchar(32)', TRUE)
+        ->Column('HS', 'varchar(32)', TRUE)
+        ->Column('Col', 'varchar(32)', TRUE)
+        ->Column('Inter', 'varchar(128)', TRUE)
+        ->Column('Mus', 'varchar(128)', TRUE)
+        ->Column('Gam', 'varchar(128)', TRUE)
+        ->Column('Mov', 'varchar(128)', TRUE)
+        ->Column('TV', 'varchar(128)', TRUE)
+        ->Column('Bks', 'varchar(128)', TRUE)
+        ->Column('Bio', 'varchar(1024)', TRUE)
+		->Column('WS', 'varchar(1024)', TRUE)
+        ->Set(FALSE, FALSE);
 	}
 }
 
 Gdn::FactoryInstall('AboutMeModel','AboutMeModel',
-PATH_PLUGINS.DS.'AboutMe'.DS.'default.php',
+PATH_PLUGINS.DS.'AboutMe'.DS.'class.aboutme.plugin.php',
 Gdn::FactoryInstance,NULL,FALSE);
-
 
 
 ?>
--- views/aboutme_edit.php
+++ views/aboutme_edit.php
@@ -17,26 +17,48 @@ echo $this->Form->Errors();
 
 // Display the form
 ?>
+<?php if(C('Plugins.AboutMe.UseRealName', TRUE) || C('Plugins.AboutMe.UseNickName', TRUE) || C('Plugins.AboutMe.UseWebSite', TRUE) || C('Plugins.AboutMe.UseRelationshipStatus', TRUE) || C('Plugins.AboutMe.UseBirthday', TRUE) || C('Plugins.AboutMe.UseQuote', TRUE) || C('Plugins.AboutMe.UseLocation', TRUE)) { ?>
 <h2>Basic Information</h2>
 <ul>
+	<?php if(C('Plugins.AboutMe.UseRealName', TRUE)) { ?>
 	 <li>
       <?php
          echo $this->Form->Label('Real Name', 'Real Name');
          echo $this->Form->TextBox('RealName');
       ?>
    </li>
+   <?php } ?>
+   
+   
+   <?php if(C('Plugins.AboutMe.UseNickName', TRUE)) { ?>
     <li>
       <?php
          echo $this->Form->Label('Nick Name', 'Nick Name');
          echo $this->Form->TextBox('OtName');
       ?>
    </li>
+   <?php } ?>
+   
+   <?php if(C('Plugins.AboutMe.UseWebSite', TRUE)) { ?>
+   <li>
+      <?php
+         echo $this->Form->Label('WebSite', 'WS');
+         echo $this->Form->TextBox('WS') ?>
+   </li>
+    <?php } ?>
+   
+   
+   <?php if(C('Plugins.AboutMe.UseRelationshipStatus', TRUE)) { ?>
     <li>
       <?php
          echo $this->Form->Label('Relationship Status', 'Relationship Status');
          echo $this->Form->DropDown('RelStat', $RelationshipData);
       ?>
    </li>
+   <?php } ?>
+   
+   
+   <?php if(C('Plugins.AboutMe.UseBirthday', TRUE)) { ?>
    <li>
       <?php
          echo $this->Form->Label('Birthday', 'Birthday');
@@ -59,94 +81,165 @@ echo $this->Form->Errors();
 			</tr>
 		</table>
 	</li>
+	 <?php } ?>
+	 
+	 
+	<?php if(C('Plugins.AboutMe.UseQuote', TRUE)) { ?>
    <li>
       <?php
          echo $this->Form->Label('Favorite Quote', 'Quote');
          echo $this->Form->TextBox('Quote', array('MultiLine' => true, 'rows'=>'20', 'cols'=>'80'));
       ?>
    </li>
+    <?php } ?>
+	
+	
+   <?php if(C('Plugins.AboutMe.UseLocation', TRUE)) { ?>
    <li>
     <?php
          echo $this->Form->Label('Location', 'Location');
          echo $this->Form->TextBox('Loc'); ?>
    </li>
+   <?php } ?>
+   
     <?php 
     $this->FireEvent('EditBasicAfter'); 
     ?>
-    <li>
+    </ul>
+	<?php } ?>
+	
+	
+	<?php if(C('Plugins.AboutMe.UseEmployer', TRUE) || C('Plugins.AboutMe.UseJobTitle', TRUE)) { ?>
    		<H2>Career</H2>
-   </li>
+   <ul>
+   
+   
+   <?php if(C('Plugins.AboutMe.UseEmployer', TRUE)) { ?>
     <li>
       <?php
          echo $this->Form->Label('Employer', 'Employer');
          echo $this->Form->TextBox('Emp');
       ?>
    </li>
+    <?php } ?>
+	
+	
+   <?php if(C('Plugins.AboutMe.UseJobTitle', TRUE)) { ?>
     <li>
       <?php
          echo $this->Form->Label('Job Title', 'Job Title');
          echo $this->Form->TextBox('JobTit');
       ?>
    </li>
-    <li>
+    <?php } ?>
+    </ul>
+	<?php } ?>
+	
+	<?php if(C('Plugins.AboutMe.UseHighSchool', TRUE) || C('Plugins.AboutMe.UseCollege', TRUE)) { ?>
    		<H2>Education</H2>
-   </li>
+   <ul>
+   
+   
+   <?php if(C('Plugins.AboutMe.UseHighSchool', TRUE)) { ?>
     <li>
       <?php
          echo $this->Form->Label('High School', 'High School');
          echo $this->Form->TextBox('HS');
       ?>
    </li>
+    <?php } ?>
+	
+	
+   <?php if(C('Plugins.AboutMe.UseCollege', TRUE)) { ?>
     <li>
       <?php
          echo $this->Form->Label('College', 'College');
          echo $this->Form->TextBox('Col');
       ?>
    </li>
+    <?php } ?>
+	
+	
    <?php 
     $this->FireEvent('EditEduAfter'); 
     ?>
-   <li>
+	</ul>
+	<?php } ?>
+	
+	
+	<?php if(C('Plugins.AboutMe.UseBio', TRUE)) { ?>
    		<H2>Biography</H2>
-   </li>
+   <ul>
    <li>
       <?php
          echo $this->Form->Label('Biography', 'Bio');
          echo $this->Form->TextBox('Bio', array('MultiLine' => true, 'rows'=>'30', 'cols'=>'80')) ?>
    </li>
-   <li>
+   </ul>
+    <?php } ?>
+	
+	
+   <?php if(C('Plugins.AboutMe.UseInter', TRUE) || C('Plugins.AboutMe.UseMus', TRUE) || C('Plugins.AboutMe.UseGam', TRUE) || C('Plugins.AboutMe.UseMov', TRUE) || C('Plugins.AboutMe.UseTV', TRUE) || C('Plugins.AboutMe.UseBks', TRUE)) { ?>
    		<H2>Interests</H2>
-   </li>
+   <ul>
+   
+   
+   <?php if(C('Plugins.AboutMe.UseInter', TRUE)) { ?>
     <li>
       <?php
          echo $this->Form->Label('Interests', 'Inter');
          echo $this->Form->TextBox('Inter') ?>
    </li>
+    <?php } ?>
+	
+	
+   <?php if(C('Plugins.AboutMe.UseMus', TRUE)) { ?>
    <li>
       <?php
          echo $this->Form->Label('Music', 'Mus');
          echo $this->Form->TextBox('Mus') ?>
    </li>
+    <?php } ?>
+	
+	
+   <?php if(C('Plugins.AboutMe.UseGam', TRUE)) { ?>
    <li>
       <?php
          echo $this->Form->Label('Games', 'Gam');
          echo $this->Form->TextBox('Gam') ?>
    </li>
+    <?php } ?>
+	
+	
+   <?php if(C('Plugins.AboutMe.UseMov', TRUE)) { ?>
    <li>
       <?php
          echo $this->Form->Label('Movies', 'Mov');
          echo $this->Form->TextBox('Mov') ?>
    </li>
+    <?php } ?>
+	
+	
+   <?php if(C('Plugins.AboutMe.UseTV', TRUE)) { ?>
    <li>
       <?php
          echo $this->Form->Label('Television', 'TV');
          echo $this->Form->TextBox('TV') ?>
    </li>
+    <?php } ?>
+	
+	
+   <?php if(C('Plugins.AboutMe.UseBks', TRUE)) { ?>
    <li>
       <?php
          echo $this->Form->Label('Books', 'Bks');
          echo $this->Form->TextBox('Bks') ?>
    </li>
+    <?php } ?>
+	</ul>
+	 <?php } ?>
+	
+	
    <?php
       $this->FireEvent('EditInterAfter');
       ?>
